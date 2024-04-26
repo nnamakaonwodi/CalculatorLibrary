2e7560975b9dd2b69783f4df0972d62c798b30d4
Edward Onwodi <edwardonwodi@Edwards-iMac.local>
2024-04-26 20:52:49 +1000

    Fix linting in python code for calc

--- test_calculator.py
+++ test_calculator.py
@@ -8,18 +8,14 @@ import calculator
 
 class TestCalculator:
 
-
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
-
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 
-
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
-
 
     def test_divide(self):
         assert 20 == calculator.divide(200, 10)
